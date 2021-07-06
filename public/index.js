e0c642c0ebd76f6039d943a5cf8b269e62becaa0
Uriel Cruz <ucruz2025@gmail.com>
2021-07-05 18:00:23 -0700

    index.js update

--- public/index.js
+++ public/index.js
@@ -1,3 +1,10 @@
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((reg) => console.log('Service Worker Registered', reg));
+  });
+}
+
 let transactions = [];
 let myChart;
 
